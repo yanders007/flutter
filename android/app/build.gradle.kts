93ccfbd71c4297c75ca3be5f8e802a16698928a5
Y <yabiander@gmail.com>
2026-06-15 18:51:33 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,12 +8,13 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-buildFeatures {
-    buildConfig = true
-}
     }
 
     defaultConfig {
